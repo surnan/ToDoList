64a699629d40739a2a0b5dc58b0ad41e755bd123
surnan <surnan@users.noreply.github.com>
2023-10-04 06:15:26 -0400

    ADD: textfield accessible to js.  Code syntax clean-up

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,12 @@
 
 
-function jsAddButtonClick(){
+function addToDo(){
+const inputElement = document.querySelector('.js-name-input');
+const name = inputElement.value;
+
+console.log('inputElement', inputElement);
+console.log('name', name);
+
+
   console.log('button clicked')
 }
